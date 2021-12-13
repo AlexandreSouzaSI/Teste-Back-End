d05313873eecd42e797a727bf4c2101e4f9eba64
Alexandre Moura <alemoura23@hotmail.com>
2021-12-13 01:21:49 -0300

    atualização 01:21

--- src/controllers/test4Controller.js
+++ src/controllers/test4Controller.js
@@ -1,11 +1,26 @@
 const { response } = require("express");
+const { fetch } = require("node-fetch");
 
 const cepTest4 = async (req, res) => {
     const { valor } = req.body;
     
     console.log(valor)
 
-    
+    // for(let i = 0; i < valor.length; i++){
+    //     let cep = valor[i];
+
+    //     const options = {
+    //         method: 'GET',
+    //         mode: 'cors',
+    //         cache: 'default'
+    //     }
+
+    //     fetch(`http://viacep.com.br/ws/${cep}/json/`, options)
+    //     .then(response => {response.json()
+    //         .then(data => console.log(data))
+    //     })
+    //     .catch(e => console.log('Deu erro: '+ e,message))
+    // }
 
     }
 
